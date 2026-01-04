db25f1e74fae241ae0243f5413d25740bec0a3dc
Zan <zhangziyan@cnstrong.cn>
2026-01-04 16:30:06 +0800

    feat(CSGO监测平台): 初始化项目

---
+++ src/main/java/com/zan/csgo/AiCsgoMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.zan.csgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCsgoMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCsgoMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zan/csgo/AiCsgoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zan.csgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCsgoMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
